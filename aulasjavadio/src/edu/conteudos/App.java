7ddd368e6dc07f1768f4fe0403bd99e3203d57fa
all0cer <italoalmeinasc@gmail.com>
2023-08-30 18:04:38 -0300

    init

---
+++ aulasjavadio/src/edu/conteudos/App.java
@@ -0,0 +1,6 @@
+package edu.conteudos; //uso de pacotes(organização)
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
