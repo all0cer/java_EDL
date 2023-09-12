27cb9c36a36c787910ec1a781417b4b6666edb1d
all0cer <italoalmeinasc@gmail.com>
2023-09-11 23:13:10 -0300

    add pilha

--- aulasjavadio/src/edu/conteudos/App.java
+++
@@ -1,6 +0,0 @@
-package edu.conteudos; //uso de pacotes(organização)
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
