7bd6a413390bed01afac6d620e81580ed9fb8b43
all0cer <italoalmeinasc@gmail.com>
2023-09-27 12:23:07 -0300

    Inserindo base para interface LL

--- filias/filas/src/ListaLigada/IListaLigada.java
+++ filias/filas/src/ListaLigada/IListaLigada.java
@@ -1,5 +1,17 @@
 package filas.src.ListaLigada;
 
-public class IListaLigada {
-    
+interface IListaLigada {
+    public  boolean isEmpty();
+    public  int size();
+    public  node first();
+    public  node last();
+    public  node before(Object node);
+    public  node after(Object node);
+    public  void replaceElement(n, o);
+    public  void swapElements(n, q);
+    public  void insertBefore(n, o);
+    public  void insertAfter(n, o);
+    public  void insertFirst(o);
+    public  void insertLast(o);
+    public  node remove(n);
 }
