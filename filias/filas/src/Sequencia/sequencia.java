6765fb546c375db0b30f200aa57b93c5b0149b44
all0cer <italoalmeinasc@gmail.com>
2023-10-08 19:06:07 -0300

    Implementando todos os métodos SQC

--- filias/filas/src/Sequencia/sequencia.java
+++ filias/filas/src/Sequencia/sequencia.java
@@ -17,82 +17,177 @@ public class sequencia implements Isequencia {
 
     @Override
     public Object elemAtRank(int index) {
-        
+    node atual = inicio.getNext();
+    for(int i=0; i<index; i++){
+        atual.getNext();
     }
+    return atual.getValor();
+}
 
     @Override
-    public void replaceAtRank(int index, Object o) {
-        
+    public void replaceAtRank(int index, Object elemento) {
+    node atual = inicio.getNext();
+    for(int i=0; i<index; i++){
+        atual.getNext();
     }
+    atual.setValor(elemento);
+}
 
     @Override
-    public void insertAfter(int index, Object o) {
+    public void insertAfter(int index, Object elemento) {
+        node new_node = new node(elemento);
+        node atual = inicio.getNext();
+        for(int i=0; i<index; i++){
+            atual.getNext();
+    }
+        new_node.setPrev(atual);
+        new_node.setNext(atual.getNext());
         
+
+        (atual.getNext()).setPrev(new_node);
+        atual.setNext(new_node);
+        ++tamanho;
     }
 
     @Override
     public void removeAtRank(int index) {
+    node atual = inicio.getNext();
+    for(int i=0; i<index; i++){
+        atual.getNext();
+    }
+        (atual.getNext()).setPrev(atual.getPrev());
+        (atual.getPrev()).setNext(atual.getNext());
+
+        atual.setNext(null);
+        atual.setPrev(null);
+        atual.setValor(null);
+        --tamanho;
+    }
+
+    @Override
+    public void replaceElement(node no, Object elemento) {
+       no.setValor(elemento);
+    }
+
+    @Override
+    public void swapElements(node no1, node no2) {
+        Object valor_no_2 = no2.getValor();
+        Object valor_no_1 = no1.getValor();
+        no1.setValor(valor_no_2);
+        no2.setValor(valor_no_1);
+    }
+
+    @Override
+    public void insertBefore(node no, Object elemento) {
+        node new_node = new node(elemento);
+        new_node.setValor(elemento);
         
+
+        new_node.setNext(no);
+        new_node.setPrev(no.getPrev());
+
+        (no.getPrev()).setNext(new_node);
+        no.setPrev(new_node);
+        ++tamanho;
     }
 
     @Override
-    public void replaceElement(node no, Object o) {
-       
-    }
-
-    @Override
-    public void swapElements(node no, node no1) {
-       
-    }
-
-    @Override
-    public void insertBefore(node no, Object o) {
+    public void insertAfter(node no, Object elemento) {
+        node new_node = new node(elemento);
+        new_node.setPrev(no);
+        new_node.setNext(no.getNext());
         
+
+        (no.getNext()).setPrev(new_node);
+        no.setNext(new_node);
+        ++tamanho;
     }
 
     @Override
-    public void insertAfter(node no, Object o) {
-       
+    public void insertFirst(Object elemento) {
+        node no = new node(elemento);
+            no.setPrev(inicio);
+            no.setNext(inicio.getNext());
+
+            (inicio.getNext()).setPrev(no);
+            inicio.setNext(no);
+            ++tamanho;
     }
 
     @Override
-    public void insertFirst(Object o) {
-       
-    }
+    public void insertLast(Object elemento) {
+        node no = new node(elemento);
+        no.setValor(elemento);
+        no.setNext(fim);
 
-    @Override
-    public void insertLast(Object o) {
-       
+        (fim.getPrev()).setNext(no);
+         fim.setPrev(no);
+         ++tamanho;
     }
 
     @Override
     public void remove(node no) {
-       
+        (no.getNext()).setPrev(no.getPrev());
+        (no.getPrev()).setNext(no.getNext());
+
+        no.setNext(null);
+        no.setPrev(null);
+        no.setValor(null);
+        --tamanho;
     }
 
     @Override
     public boolean isEmpty(){
-
+        return inicio.getNext() == fim;
     }
 
     @Override
     public long size(){
-        
+        return tamanho;
     }
 
     @Override
-    public node find(Object o) {
-       
+    public node find(Object valor) {
+        node atual = inicio.getNext();
+        while(atual.getValor() != valor){
+            atual = atual.getNext();
+        }
+        
+        return atual;
     }
 
     @Override
     public Object atRank(int index) {
-       
+    node atual = inicio.getNext();
+    for(int i=0; i<index; i++){
+        atual.getNext();
+    }
+    return atual.getValor();
     }
 
     @Override
     public Object rankOf(node no) {
-       
+       node atual = inicio.getNext();
+       long rank = 0;
+        while(atual != no){
+            atual = atual.getNext();
+            rank++;
+        }
+        
+        return rank;
     }
+
+    public void PrintVector() {
+		node atual = inicio.getNext();
+		System.out.print("[ ");
+		for(int i = 0; i < size(); i++) {
+            if(atual.getNext() == fim) {
+				break;
+			}
+			System.out.print(atual.getValor()+ ", ");
+			atual = atual.getNext();
+		}
+		System.out.println("]");
+	}
     
 }
--- filias/filas/src/Sequencia/testesequencia.java
+++ filias/filas/src/Sequencia/testesequencia.java
@@ -1,5 +1,21 @@
 package filas.src.Sequencia;
 
 public class testesequencia {
-    
+    public static void main(String[] args){ 
+        sequencia sequencia = new sequencia();
+        System.out.println("Inserções:");
+        sequencia.insertFirst(45);
+        sequencia.insertLast(9);
+        sequencia.insertAfter(sequencia.find(45), 32);
+        sequencia.insertBefore(sequencia.find(9), 89);
+        sequencia.PrintVector();
+
+        System.out.println("Remoção:");
+        sequencia.remove(sequencia.find(32));
+        sequencia.PrintVector();
+
+        System.out.println("Troca de elementos:");
+        sequencia.swapElements(sequencia.find(89), sequencia.find(9));
+        sequencia.PrintVector();
+    }
 }
